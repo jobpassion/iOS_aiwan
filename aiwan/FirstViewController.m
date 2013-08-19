be65c4b04dc8850f2d0aab63135eacddc421bd11
redrum <redrum@redrumtekiMac.local>
2013-08-19 02:26:16 -0700

    ddd

--- aiwan/FirstViewController.m
+++ aiwan/FirstViewController.m
@@ -18,6 +18,35 @@
     NSLog(@"%@", [[self userNameField] text]);
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:[@"hello " stringByAppendingString:[[self userNameField] text]] message:@"点击确定注册" delegate:NULL cancelButtonTitle:@"取消" otherButtonTitles:@"确定", nil];
     [alert show];
+    
+    
+    NSString *urlString = [NSString stringWithFormat:@"http://www.baidu.com"];
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init] ;
+    [request setURL:[NSURL URLWithString:urlString]];
+    [request setHTTPMethod:@"POST"];
+    
+    //set headers
+    NSString *contentType = [NSString stringWithFormat:@"text/xml"];
+    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+    
+    //create the body
+    NSMutableData *postBody = [NSMutableData data];
+    [postBody appendData:[[NSString stringWithFormat:@"<xml>"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postBody appendData:[[NSString stringWithFormat:@"<yourcode/>"] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postBody appendData:[[NSString stringWithFormat:@"</xml>"] dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    //post
+    [request setHTTPBody:postBody];
+    
+    //get response
+    NSHTTPURLResponse* urlResponse = nil;
+    NSError *error = [[NSError alloc] init];
+    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&urlResponse error:&error];
+    NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+    NSLog(@"Response Code: %d", [urlResponse statusCode]);
+    if ([urlResponse statusCode] >= 200 && [urlResponse statusCode] < 300) {
+        NSLog(@"Response: %@", result);
+    }
 }
 
 - (void)viewDidLoad
